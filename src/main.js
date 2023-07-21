1b0841343d4457fa1a5956547903b5771dfbd042
Swaroop Acharya <swaroopacharya935@gmail.com>
2023-07-21 14:23:38 +0530

    First commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalise.css'
+import '../styles/style.css'
+import '../styles/componets/header.css'
+import '../styles/componets/section.css'
+import '../styles/componets/footer.css'
+import '../styles/utils.css'
+
